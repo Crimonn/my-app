05fa36ab6cc90604bde2e586ee79f1b97b4da316
Crimonn <s65122519043@ssru.ac.th>
2024-08-27 11:06:08 +0700

    Toggle Image

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React, { useState } from 'react';
 function App() {
-  const [word, setWord] = useState('Hello');  // Initialize state with the word 'Hello'
+  const [image, setImage] = useState('https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_square.jpg');  // Initialize state with the word 'Hello'
 
-  const toggleWord = () => {
+  const toggleImage = () => {
     // Use an if statement to toggle between the words 'Hello' and 'Goodbye'
-    if (word === 'Hello') {
-      setWord('Goodbye');
+    if (image === 'https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_square.jpg') {
+      setImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSVdVzgFiOFLXyhq4OwGT8ac16t6JAtHjRoQ&s');
     } else {
-      setWord('Hello');
+      setImage('https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_square.jpg');
     }
   };
 
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>{word}</h1>  {/* Display the current word */}
-      <button onClick={toggleWord}>Toggle Word</button>  {/* Button to toggle the word */}
+      {/* Display the current image */}
+      <img src={image} alt="Toggle" style={{ width: '50%', maxWidth: '500px' }} />
+      {/* Button to toggle the image */}
+      <div>
+        <button onClick={toggleImage} style={{ marginTop: '20px' }}>JUMPSCARE!!!</button>
+      </div>
     </div>
   );
 }
